5cc57664e1f53c52f4119b59fadb2525c269d2ec
poojaa113 <51355020+poojaa113@users.noreply.github.com>
2019-07-25 12:51:38 +0530

    Create pros24.py

---
+++ pros24.py
@@ -0,0 +1,13 @@
+vj8,vk8=map(int,input().split())
+l8=list(map(int,input().split()))
+vj8=[]
+l8.insert(0,0)
+for a in range(vk8):
+     v8=[]
+     sumup=0
+     ccc,ddd=map(int,input().split())
+     for bb in range(ccc,ddd+1):         
+         sumup^=l8[bb]     
+     vj8.append(sumup)
+for cc in vj8:
+    print(cc)
